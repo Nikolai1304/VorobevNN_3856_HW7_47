c7c9c84410492a4a00cc54794af9169885e07220
Nikolai <agro034@mail.ru>
2023-02-23 01:26:25 +0300

    47

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿int numRows = SetNumber("Rows ");
+int numColumns = SetNumber("Columns ");
+
+int numMaxValue = SetNumber("Max number ");
+int numMinValue = SetNumber("Min number ");
+
+double[,] matrix = GetMatrix(numRows, numColumns, numMinValue, numMaxValue);
+Print(matrix);
+System.Console.WriteLine();
+
+
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+double [,] GetMatrix(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = new Random().Next(min, max + 1)/10.0;
+        }
+    }
+    return matrix;
+}
+
+void Print(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            System.Console.Write(matrix[i, l] + "         ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
